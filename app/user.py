9b30d8aae917067168fdc5347d399a83e12929c3
mishkozen <mkozenkow@gmail.com>
2026-05-24 07:04:52 +0200

    Some changes

---
+++ app/user.py
@@ -0,0 +1,4 @@
+class User:
+    def __init__(self, name, wallet):
+            self._name = name
+            self._wallet = wallet
---
+++ app/wallet.py
@@ -0,0 +1,5 @@
+class Wallet:
+    def __init__(self, balance=0):
+        self._balance = balance
+        self._transactions = []
+
